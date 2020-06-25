3d0336a3667fe1f0446f32ff78d06c22d0a2cd65
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-06-24 22:47:58 -0500

    Complete restructure, added new data card

--- src/index.js
+++ src/index.js
@@ -17,16 +17,47 @@ function MyNav() {
   );
 }
 
-function InfoCard({ name, location, age, description }) {
+const cardData = [
+  {
+    id: "1",
+    imgsrc: "https://i.ibb.co/Lx9PLPx/ph-logo-mark-white-space.jpg",
+    imgalt: "Pixl Haus logo",
+    name: "Pixl Haus",
+    category: "web design",
+    tag1: "freelancer",
+    tag2: "creative"
+  }
+];
+
+function InfoCard({ dataRender }) {
   return (
-    <div className="info_card">
-      <h2>{name}</h2>
-      <h3>{location}</h3>
-      <h4>{age}</h4>
-      <p>{description}</p>
+    <div className="business_card_container">
+      {dataRender.map(data => (
+        <div>
+          <img src={data.imgsrc} alt={data.imgalt} border="0" />
+          <h2>{data.name}</h2>
+          <p className="business_card_category">{data.category}</p>
+          <ul className="business_card_tags">
+            <li>
+              <p>{data.tag1}</p>
+            </li>
+            <li>
+              <p>{data.tag2}</p>
+            </li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
 
+function FooterText() {
+  return <p className="footer_text">This site is built with ReactJS</p>;
+}
+
 ReactDOM.render(<MyNav />, document.getElementById("area_nav"));
-ReactDOM.render(<InfoCard />, document.getElementById("area_body_content"));
+ReactDOM.render(
+  <InfoCard dataRender={cardData} />,
+  document.getElementById("cards_container")
+);
+ReactDOM.render(<FooterText />, document.getElementById("footer"));
