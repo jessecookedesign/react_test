7f643c1828a08fc5dec4e02e5c5f527ab688f45b
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-06-28 00:42:14 -0500

    Added additional business info

--- src/index.js
+++ src/index.js
@@ -34,6 +34,12 @@ const cardData = [
     imgalt: "Pixl Haus logo",
     name: "Pixl Haus",
     category: "web design",
+    address: "702 E 3rd St, Duluth, MN 55805",
+    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+    website: "example.com",
+    websitelink: "https://www.google.com",
+    phone: "218-555-1234",
+    phonelink: "tel: 1-218-555-5555",
     tag1: "freelancer",
     tag2: "creative"
   },
@@ -43,17 +49,40 @@ const cardData = [
     imgalt: "Duluth Screen Printing Co logo",
     name: "Duluth Screen Printing Co.",
     category: "printing",
+    address: "702 E 3rd St, Duluth, MN 55805",
+    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+    website: "example.com",
+    websitelink: "https://www.google.com",
+    phone: "218-555-1234",
+    phonelink: "tel: 1-218-555-5555",
     tag1: "affordable",
     tag2: "eco-friendly"
   },
   {
     id: "3",
     imgsrc: "https://i.ibb.co/SDg1gcW/yourlogohere.jpg",
-    imgalt: "",
+    imgalt: "Your logo here",
     name: "Business Name",
     category: "category here",
+    address: "702 E 3rd St, Duluth, MN 55805",
+    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+    website: "example.com",
+    websitelink: "https://www.google.com",
+    phone: "218-555-1234",
+    phonelink: "tel: 1-218-555-5555",
     tag1: "tag one",
     tag2: "tag two"
+  },
+  {
+    id: "4",
+    name: "Business Name",
+    category: "category here",
+    address: "702 E 3rd St, Duluth, MN 55805",
+    addresslink: "http://maps.google.com/?q=702 e 3rd st duluth mn 55805",
+    website: "example.com",
+    websitelink: "https://www.google.com",
+    phone: "218-555-1234",
+    phonelink: "tel: 1-218-555-5555"
   }
 ];
 
@@ -66,6 +95,23 @@ function InfoCard({ dataRender }) {
             <img src={data.imgsrc} alt={data.imgalt} border="0" />
             <h2>{data.name}</h2>
             <p className="business_card_category">{data.category}</p>
+            <ul className="business_card_info">
+              <li>
+                <a href={data.addresslink}>
+                  <p>{data.address}</p>
+                </a>
+              </li>
+              <li>
+                <a href={data.websitelink}>
+                  <p>{data.website}</p>
+                </a>
+              </li>
+              <li>
+                <a href={data.phonelink}>
+                  <p>{data.phone}</p>
+                </a>
+              </li>
+            </ul>
             <ul className="business_card_tags">
               <li>
                 <p>{data.tag1}</p>
